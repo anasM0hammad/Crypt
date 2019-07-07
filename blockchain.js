22956a7263e537dcf78b69fb3480129b0f4dd34b
anasM0hammad <anas.1633.m@gmail.com>
2019-07-07 16:26:36 +0530

    blockchain validation method added

--- blockchain.js
+++ blockchain.js
@@ -1,4 +1,5 @@
 const Block = require('./block') ;
+const cryptoHash = require('./crypto-hash') ;
 
 class Blockchain{
     constructor(){
@@ -10,6 +11,28 @@ class Blockchain{
         const newBlock = Block.mineBlock({data : data , lastBlock : lastBlock }) ;
 
         this.chain.push(newBlock) ;
+    }
+
+    static isValid(chain){
+       if(JSON.stringify(chain[0]) !== JSON.stringify(Block.genesis())) 
+        return false ;
+        
+       for(let i=1 ; i<chain.length ; i++){
+           const block = chain[i] ;
+           const lastHash = chain[i-1].hash ;
+           const data = block.data ;
+           const timestamp = block.timestamp ;
+           const hash = block.hash ;
+           
+           if(block.lastHash !== lastHash)
+            return false ;
+
+           if(cryptoHash(timestamp , lastHash , data) !== hash)
+            return false ; 
+       }  
+
+       return true ;
+
     }
 }
 
--- blockchain.test.js
+++ blockchain.test.js
@@ -3,7 +3,11 @@ const Blockchain = require('./blockchain') ;
 
 describe('Blockchain', ()=>{
 
-    const blockchain = new Blockchain() ;
+    let blockchain  ;
+
+    beforeEach(() => {
+        blockchain = new Blockchain() ;
+    });
 
     it('contains a chain of type array' , ()=>{
         expect(blockchain.chain instanceof Array).toBe(true) ;
@@ -17,6 +21,46 @@ describe('Blockchain', ()=>{
         const newData = 'foo' ;
         blockchain.addBlock({data : newData}) ;
         expect(blockchain.chain[blockchain.chain.length-1].data).toEqual(newData) ;
+    });
+
+
+    describe('isValid()' , ()=>{
+
+        beforeEach(() => {
+            blockchain = new Blockchain() ;
+            blockchain.addBlock({data : 'jon'}) ;
+            blockchain.addBlock({data : 'sansa'}) ;
+            blockchain.addBlock({data : 'arya'}) ;
+        });
+
+        describe('When chain does not start with genesis block' , ()=>{
+            it('returns false', ()=>{
+                blockchain.chain[0] = {data : 'fake-genesis'} ;
+                expect(Blockchain.isValid(blockchain.chain)).toBe(false) ;
+            }) ;
+        });
+
+        describe('When chain does start with genesis block having multiple blocks' , ()=>{
+            describe('and last hash not matches the hash of last block' , ()=>{
+                it('return false' , () =>{
+                    blockchain.chain[2].lastHash = 'fake-hash' ;
+                    expect(Blockchain.isValid(blockchain.chain)).toBe(false) ;
+                }) ;
+            });
+
+            describe('and chain has a block with invalid inputs ' , ()=> {
+                it('return false' , () => {
+                    blockchain.chain[2].data = 'bad-data' ;
+                    expect(Blockchain.isValid(blockchain.chain)).toBe(false) ;
+                }) ;
+            }) ;
+
+            describe('and chain does not contain any invalid block' , ()=>{
+                it('return true' , () =>{ 
+                    expect(Blockchain.isValid(blockchain.chain)).toBe(true) ;
+                }) ;
+            }) ;
+        });
     });
 
 });
