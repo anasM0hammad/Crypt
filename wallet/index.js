3f6e942facd4795f902b59e6132529ed97762c76
anasM0hammad <anas.1633.m@gmail.com>
2019-07-24 14:40:21 +0530

    generating public key using elliptic algorithms

--- wallet/index.js
+++ wallet/index.js
@@ -1,8 +1,12 @@
+const { ec } = require('../utli') ;
 const { STARTING_BALANCE } = require('../config') ;
 
 class Wallet{
     constructor(){
+        const keyPair = ec.genKeyPair() ;
+
         this.balance = STARTING_BALANCE ;
+        this. publicKey = keyPair.getPublic().encode('hex') ;
     }
 }
 
