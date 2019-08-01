0891c60e556b03546446b930d82d067ede4ee76d
anasM0hammad <anas.1633.m@gmail.com>
2019-08-01 15:52:33 +0530

    calculate balance method in wallet

--- wallet/index.js
+++ wallet/index.js
@@ -23,6 +23,24 @@ class Wallet{
         return new Transaction({senderWallet : this , recipient , amount}) ;
 
     }
+
+    static calculateBalance({chain , address}){
+
+        let output_total = 0 ;
+
+        for(let i = 1 ; i<chain.length ; i++){
+            
+            let block = chain[i] ;
+
+            for(let transaction of block.data){
+                if(transaction.outputMap[address]){
+                    output_total += transaction.outputMap[address] ;
+                }
+            }
+        }
+
+        return STARTING_BALANCE + output_total ;
+    }
 }
 
 module.exports = Wallet ;
--- wallet/index.test.js
+++ wallet/index.test.js
@@ -1,6 +1,8 @@
 const Wallet = require('./index') ;
 const { verifySignature } = require('../util') ;
 const Transaction = require('./transaction') ;
+const Blockchain = require('../blockchain') ;
+const { STARTING_BALANCE } = require('../config') ;
 
 describe('Wallet' , () => {
     let wallet ;
@@ -78,4 +80,47 @@ describe('Wallet' , () => {
         }) ;
     }) ;
 
+    describe('calculateBalance()' , () =>{
+
+        let blockchain ;
+
+        beforeEach(() => {
+            blockchain = new Blockchain() ;
+        });
+
+        describe('when there is no output for the wallet' , () => {
+            it('returns the `STARTING_BALANCE` ' , () => {
+                expect(Wallet.calculateBalance({
+                    chain : blockchain.chain ,
+                    address : wallet.publicKey
+                })).toEqual(STARTING_BALANCE) ;
+            }) ;
+        });
+
+        describe('when there is output for the wallet' , () => {
+
+            let transaction1 , transaction2 ;
+            beforeEach(() => {
+                transaction1 = new Wallet().createTransaction({
+                    recipient : wallet.publicKey ,
+                    amount : 50 
+                });
+                transaction2 = new Wallet().createTransaction({
+                    recipient : wallet.publicKey ,
+                    amount : 60 
+                });
+
+                blockchain.addBlock({data : [transaction1 , transaction2]}) ;
+            }); 
+
+            it('add the sum of all output to the wallet ' , () => {
+                expect(Wallet.calculateBalance({
+                    chain : blockchain.chain ,
+                    address : wallet.publicKey
+                }))
+                .toEqual(STARTING_BALANCE + transaction1.outputMap[wallet.publicKey] + transaction2.outputMap[wallet.publicKey]) ;
+            });
+
+        }) ;
+    });
 }) ;
